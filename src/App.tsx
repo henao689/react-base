61ebe1b51fc606e0772a9a30eae4cd46e3d15e91
mhenao <imac21@MacBook-Pro-de-iMac.local>
2022-06-05 14:14:44 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import Navigation from './routes/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation/>
+    </>
+  );
+}
+
+export default App;
